e81ec281fb401dd3a5650b8bae9a4e04a9cf7f9c
Aarni <aarni.miikkulainen@helsinki.fi>
2024-06-27 14:21:20 +0300

    part 2 weather added, part 3 fix

--- full_stack_part3/src/index.js
+++ full_stack_part3/src/index.js
@@ -1,11 +1,9 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
-import "./index.css"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById("root")).render(
-  <App />
-)
+const root = ReactDOM.createRoot(document.getElementById('root')).render(<App />)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- osa_2/puhelinluettelo/src/App.js
+++ osa_2/puhelinluettelo/src/App.js
@@ -1,41 +1,36 @@
-import Filter from './components/filter'
-import PersonForm from './components/personform'
-import ShowPersons from './components/ShowPersons'
-import Notification from './components/Notification'
-import personService from './services/persons'
-import { useState, useEffect } from 'react'
-
-
+import Filter from "./components/filter"
+import PersonForm from "./components/personform"
+import ShowPersons from "./components/ShowPersons"
+import Notification from "./components/Notification"
+import personService from "./services/persons"
+import { useState, useEffect } from "react"
 
 const App = () => {
-  const [persons, setPersons] = useState([]) 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [newFilter, setNewFilter] = useState('')
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState("")
+  const [newNumber, setNewNumber] = useState("")
+  const [newFilter, setNewFilter] = useState("")
   const [errorMessage, setErrorMessage] = useState(null)
   const [isError, setIsError] = useState(false)
 
   useEffect(() => {
-    console.log('effect')
-    personService
-      .getAll()
-      .then(response => {
-        console.log('promise fulfilled')
-        setPersons(response)
-      })
+    console.log("effect")
+    personService.getAll().then((response) => {
+      console.log("promise fulfilled")
+      setPersons(response)
+    })
   }, [])
-  console.log('render', persons.length, 'persons')
+  console.log("render", persons.length, "persons")
 
   const deletePerson = (person) => {
     if (window.confirm(`Delete ${person.name} ?`)) {
-      personService 
-      .deletePerson(person.id)
-      .then(() => {
-        setPersons(deletedPerson => deletedPerson.filter(({id}) => id !== person.id))
+      personService.deletePerson(person.id).then(() => {
+        setPersons((deletedPerson) =>
+          deletedPerson.filter(({ id }) => id !== person.id)
+        )
       })
-      setErrorMessage(
-        `${person.name}'s number successfully deleted!`
-      )
+      setErrorMessage(`${person.name}'s number successfully deleted!`)
+      setIsError(false)
       setTimeout(() => {
         setErrorMessage(null)
       }, 5000)
@@ -44,23 +39,29 @@ const App = () => {
 
   const addPerson = (event) => {
     event.preventDefault()
-    const names = persons.map(person=>person.name)
+    const names = persons.map((person) => person.name)
     const personObject = {
       name: newName,
-      number: newNumber
+      number: newNumber,
     }
     if (names.includes(newName)) {
-        if (window.confirm(` ${newName} is already added to phonebook, 
-        replace the old number with a new one?`)){
-          const personId = persons[names.indexOf(newName)].id
-          personService
+      if (
+        window.confirm(` ${newName} is already added to phonebook, 
+        replace the old number with a new one?`)
+      ) {
+        const personId = persons[names.indexOf(newName)].id
+        personService
           .update(personId, personObject)
-          .then(returnedPerson =>{
-            setPersons(persons.map(person => person.id !== personId ? person : returnedPerson))
-            setNewName('')
-            setNewNumber('')
+          .then((returnedPerson) => {
+            setPersons(
+              persons.map((person) =>
+                person.id !== personId ? person : returnedPerson
+              )
+            )
+            setNewName("")
+            setNewNumber("")
           })
-          .catch(error => {
+          .catch((error) => {
             setErrorMessage(
               `'${newName}' was already deleted from server, please update page`
             )
@@ -68,32 +69,45 @@ const App = () => {
             setTimeout(() => {
               setErrorMessage(null)
             }, 5000)
-    
           })
-          setErrorMessage(
-            `${newName}'s number successfully changed!`
-          )
-          setIsError(false)
-          setTimeout(() => {
-            setErrorMessage(null)
-          }, 5000)
-        } else {
-        setNewName('')
-        setNewNumber('')
-        }
+        setErrorMessage(`${newName}'s number successfully changed!`)
+        setIsError(false)
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+      } else {
+        setNewName("")
+        setNewNumber("")
+      }
     } else {
-
-    console.log("123")
-    personService
-      .create(personObject)
-      .then(returnedPerson => {
-        setPersons(persons.concat(returnedPerson))
-        setNewName('')
-        setNewNumber('')
-      })
-      setErrorMessage(
-        `${newName} successfully added to phonebook!`
-      )
+      personService
+        .create(personObject)
+        .then((returnedPerson) => {
+          setPersons(persons.concat(returnedPerson))
+          setNewName("")
+          setNewNumber("")
+        })
+        .catch((error) => {
+          console.log("catchattu")
+          if (newName.length > 3) {
+            setErrorMessage(
+              "Number must be at least 8 characters long and formatted correctly!"
+            )
+            setIsError(true)
+            setTimeout(() => {
+              setErrorMessage(null)
+            }, 5000)
+          } else {
+            console.log("lyhyt nimi")
+            setErrorMessage("Name must be at least 3 characters long!")
+            setIsError(true)
+            setTimeout(() => {
+              setErrorMessage(null)
+            }, 5000)
+          }
+          console.log(error.response.data)
+        })
+      setErrorMessage(`${newName} successfully added to phonebook!`)
       setIsError(false)
       setTimeout(() => {
         setErrorMessage(null)
@@ -114,31 +128,28 @@ const App = () => {
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <Notification message = {errorMessage} error = {isError} />
-      <Filter newFilter = {newFilter} handleFilterChange = {handleFilterChange}/>
+      <h2>Phonelook</h2>
+      <Notification message={errorMessage} error={isError} />
+      <Filter newFilter={newFilter} handleFilterChange={handleFilterChange} />
       <h3>Add new</h3>
-      
-      <PersonForm 
-      addPerson = {addPerson}
-      newName = {newName} 
-      handleNameChange={handleNameChange}
-      newNumber = {newNumber}
-      handleNumberChange={handleNumberChange}
+
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
       />
 
       <h2>Numbers</h2>
-      
-        <ShowPersons
+
+      <ShowPersons
         persons={persons}
         filter={newFilter}
         deletePerson={deletePerson}
-        />
-
+      />
     </div>
   )
-
 }
 
 export default App
-
