f1963911f0716e7252facf228f2ae8440eb8afce
Aarni <aarni.miikkulainen@helsinki.fi>
2025-03-17 14:01:42 +0200

    finish unicafe and anecdotes

--- part_1/unicafe/src/App.jsx
+++ part_1/unicafe/src/App.jsx
@@ -2,17 +2,36 @@ import { useState } from 'react'
 
 const Button = (props) => <button onClick={props.onClick}>{props.text}</button>
 
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
 const Statistics = ({ good, neutral, bad }) => {
   if (good + neutral + bad != 0) {
     return (
       <div>
         <h1>statistics</h1>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {good + neutral + bad}</p>
-        <p>average {(good - bad) / (good + neutral + bad)}</p>
-        <p>positive {(good / (good + neutral + bad)) * 100} %</p>
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={good} />
+            <StatisticLine text="neutral" value={neutral} />
+            <StatisticLine text="bad" value={bad} />
+            <StatisticLine text="all" value={good + neutral + bad} />
+            <StatisticLine
+              text="average"
+              value={(good - bad) / (good + neutral + bad)}
+            />
+            <StatisticLine
+              text="positive"
+              value={(good / (good + neutral + bad)) * 100 + '%'}
+            />
+          </tbody>
+        </table>
       </div>
     )
   }
